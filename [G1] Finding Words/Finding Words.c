a717c49634baa1ba45a7c1aebe849d2e9d768a36
Maciej Dudek <maciejdudek96@wp.pl>
2018-04-17 18:31:01 +0200

    Ready correct file [G1]

--- [G1] Finding Words/Finding Words.c
+++ [G1] Finding Words/Finding Words.c
@@ -5,14 +5,44 @@
 
 int main(void) {
 
-    char linia[61];
-    fgets(linia, 60, stdin);
-    while(strlen(linia) != 2 || linia[0] != '#') {
+    char line[61], word[61];
+    int i, j, transfer = 0; /* if in the end of line is '-' change to 1 */
+    int lineLength, wordLength = 0;
 
-        printf("%s\n", linia);
+    fgets(line, 61, stdin);
 
+    while(strlen(line) != 2 || line[0] != '#') { /* string format: ...\0 */
 
-        fgets(linia, 60, stdin);
+        lineLength = strlen(line);
+
+        for(i = 0; i < lineLength; ++i) {
+
+            if(isalpha(line[i])) {  /* if 'normal' letter */
+
+                word[wordLength] = line[i];
+                ++wordLength;
+
+            } else if(line[i] == '-') { /* if word transfer */
+
+                transfer = 1;
+                break;
+
+            } else if(isspace(line[i])) { /* end of word */
+
+                for(j = 0; j < wordLength; ++j) printf("%c", word[j]);
+
+                if(transfer == 1) {
+                    puts("");
+                    transfer = 0;
+                }
+
+                printf("%c", line[i]);
+                wordLength = 0;
+
+            } /* else do nothing */
+        }
+
+        fgets(line, 61, stdin);
     }
 
     return 0;
