d6c72b7085c8d76aa858acebaac1e47ab4782437
Maciej Dudek <maciejdudek96@wp.pl>
2018-05-16 11:19:10 +0200

    Add function coefficients( ) and edit structure LineSegment

--- [E2] Determine the Shape/[E2] Function version.c
+++ [E2] Determine the Shape/[E2] Function version.c
@@ -5,17 +5,64 @@ typedef struct Point {
     int y;
 } Point;
 
-typedef struct lineSegment {
-    Point point;
+typedef struct LineSegment {
+    Point pointA;
+    Point pointB;
     int length; /* squared, because it will be Integer */
-} lineSegment;
+    int A, B, C; /* coefficients of the general form of a square function */
+} LineSegment;
+
+int lineSegmentLength(Point A, Point B);
+
+void coefficients(LineSegment *line);
+
+void changePoints(Point *A, Point *B);
+
+int whereIsPointC(Point A, Point B, Point C);
+
+int lineSegmentIntsect(LineSegment first, LineSegment second);
+
+int perpendicularLines(LineSegment first, LineSegment second);
+
+int parallelLines(LineSegment first, LineSegment second);
+
+int main(void) {
+
+    Point pierwszy;
+    Point drugi;
+    pierwszy.x = 2;
+    pierwszy.y = 5;
+    drugi.x = 5;
+    drugi.y = 9;
+
+    LineSegment prosta;
+    prosta.pointA = pierwszy;
+    prosta.pointB = drugi;
+
+    coefficients(&prosta);
+
+    printf("%d, %d, %d", prosta.A, prosta.B, prosta.C);
+
+    return 0;
+}
+
+int lineSegmentLength(Point A, Point B) {
+    int length;
+    length = (B.x - A.x)*(B.x - A.x) + (B.y - A.y)*(B.y - A.y);
+    return length;
+}
 
 void changePoints(Point *A, Point *B) {
     Point temp;
-
     temp = *A;
     *A = *B;
     *B = temp;
+}
+
+void coefficients(LineSegment *line) {
+    line->A = line->pointA.y - line->pointB.y;
+    line->B = line->pointB.x - line->pointA.x;
+    line->C = (line->pointA.x * line->pointB.y) - (line->pointB.x * line->pointA.y);
 }
 
 int whereIsPointC(Point A, Point B, Point C) {
@@ -27,40 +74,17 @@ int whereIsPointC(Point A, Point B, Point C) {
     return 0;
 }
 
-int lineSegmentIntsect(lineSegment first, lineSegment second) {
+int lineSegmentIntsect(LineSegment first, LineSegment second) {
     /*sprawdza czy dwa odcinki sie przecinaja, 0 jesli nie, 1 jesli tak*/
     return 0;
 }
 
-int perpendicularLines(lineSegment first, lineSegment second) {
+int perpendicularLines(LineSegment first, LineSegment second) {
     /*sprawdza czy dwa odcinki sa prostopadle, 0 jesli nie, 1 jesli tak*/
     return 0;
 }
 
-int parallelLines(lineSegment first, lineSegment second) {
+int parallelLines(LineSegment first, LineSegment second) {
     /*sprawdza czy dwa odcinki sa rownolegle, 0 jesli nie, 1 jesli tak*/
-    return 0;
-}
-
-int lineSegmentLength(Point A, Point B) {
-    int length;
-
-    length = (B.x - A.x)*(B.x - A.x) + (B.y - A.y)*(B.y - A.y);
-
-    return length;
-}
-
-int main(void) {
-
-/*test*/
-    Point pierwszy;
-    Point drugi;
-    pierwszy.x = 2;
-    pierwszy.y = 5;
-    drugi.x = 5;
-    drugi.y = 9;
-
-    printf("Dlugosc odcinka do kwadratu to: %d", lineSegmentLength(pierwszy, drugi));
-
     return 0;
 }
