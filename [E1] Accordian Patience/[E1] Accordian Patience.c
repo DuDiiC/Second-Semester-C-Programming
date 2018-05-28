81e307d11d6b3e0537faf493179da4075079dae6
Maciej Dudek <maciejdudek96@wp.pl>
2018-05-28 22:13:34 +0200

    Not working implementation

--- [E1] Accordian Patience/[E1] Accordian Patience.c
+++ [E1] Accordian Patience/[E1] Accordian Patience.c
@@ -1,1 +1,84 @@
+#include <stdio.h>
 
+#define FOR(x, b, e) for(x = b; x <= (e); ++x)
+#define FORD(x, b, e) for(x = b; x >= (e); ––x)
+#define REP(x, n) for(x = 0; x < (n); ++x)
+
+typedef struct Stack {
+    int x;
+    int y;
+} Stack;
+
+int main(void) {
+
+    int i, j, k;
+    char card[3];
+    Stack s[52][52];
+    int tops[52];
+    int howMuch;
+    int yesOrNo;
+
+    while(scanf("%s", card) && card[0] != '#') { /* loading the first card */
+
+        s[0][0].x = card[0];
+        s[0][0].y = card[1];
+
+        FOR(i, 1, 51) { /* loading of other cards */
+            scanf("%s", card);
+            s[i][0].x = card[0];
+            s[i][0].y = card[1];
+        }
+
+        REP(i, 52) tops[i] = 0;
+        howMuch = 52;
+
+        while(1) {
+            yesOrNo = 0;
+            REP(i, howMuch) {
+                if(i >= 3) { /* fits three befofe */
+                    if(s[i][tops[i]].x == s[i-3][tops[i-3]].x ||
+                       s[i][tops[i]].y == s[i-3][tops[i-3]].y) {
+                            /* change position of card */
+                            tops[i-3]++;
+                            s[i-3][tops[i-3]] = s[i][tops[i]];
+                            tops[i]--;
+                            yesOrNo = 1;
+                       }
+                } else if(i >= 1) { /*fits one before */
+                    if(s[i][tops[i]].x == s[i-1][tops[i-1]].x ||
+                       s[i][tops[i]].y == s[i-1][tops[i-1]].y) {
+                            /* change position of card */
+                            tops[i-1]++;
+                            s[i-1][tops[i-1]] = s[i][tops[i]];
+                            tops[i]--;
+                            yesOrNo = 1;
+                       }
+                }
+
+                if(tops[i] == -1) {
+                    FOR(j, i+1, howMuch-1) {
+                        REP(k, tops[j]+1) {
+                            s[j-1][k] = s[j][k];
+                        }
+                        tops[j-1] = tops[j];
+                    }
+                    howMuch--;
+                }
+                if(yesOrNo == 1) break;
+            }
+            if(!yesOrNo) break;
+        }
+        /* output */
+        if(howMuch-1 == 0) {
+            printf("%d pile remaining: ", howMuch);
+        } else {
+            printf("%d piles remaining: " );
+        }
+        REP(i, howMuch-2) {
+            printf("%d ", tops[i]+1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
